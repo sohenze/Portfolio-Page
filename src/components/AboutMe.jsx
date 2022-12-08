885c3d86adeb71e09c5cdb9f3206fd094321b520
sohenze <e0725494@u.nus.edu>
2022-12-08 19:55:40 +0800

    Refactor AboutMe section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,13 +2,15 @@ import {UserData} from "../data/UserData"
 
 export const AboutMe = () => {
     return (
-      <div id='AboutMe'>
+      <div className='AboutMe' id='AboutMe'>
         <h1> About Me </h1>
-        <p style = {{fontSize: '20px'}}>
-          My name is {UserData.firstName} and I'm a year 2 Computer Science major at the National University of Singapore. 
-          <br></br>While I am relatively inexeperienced, I am adaptable and always passionate about learning more! 
-          <br></br>I hope the projects here will help show that :)
-        </p>
+        <div className="AboutMeDesc">
+          <p>
+            My name is {UserData.firstName} and I'm a year 2 Computer Science major at the National University of Singapore. 
+            While I am relatively inexeperienced, I am adaptable and always passionate about learning more! 
+            <br></br>I hope the projects here will help show that :)
+          </p>
+        </div>
       </div>
     )
 }
